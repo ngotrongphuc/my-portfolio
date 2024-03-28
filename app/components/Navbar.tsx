b9174113cb13b3c09546be4847990f5f8de742e5
ngo trong phuc <phucngo4499@gmail.com>
2024-03-28 21:24:52 +0700

    make responsive nabar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,28 +7,57 @@ import debounce from 'lodash/debounce';
 import useNavbarVisible from '../hooks/useNavbarVisible';
 import { navbarItems } from '../utils/constants';
 import { NavbarItem } from '../utils/types';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 
 const Navbar = () => {
   const isNavbarVisible = useNavbarVisible();
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuVisible(!isMenuVisible);
+  };
+
+  useEffect(() => {
+    if (!isNavbarVisible) {
+      setIsMenuVisible(false);
+    }
+  }, [isNavbarVisible]);
 
   return (
     <header
-      className={`flex fixed w-full min-h-16 items-center justify-between p-6 z-50 ${!isNavbarVisible ? '-translate-y-full' : 'translate-y-0'} transition-all duration-1000 ease-in-out`}
+      className={`flex fixed w-full h-[70px] items-center justify-between p-6 z-50 ${!isNavbarVisible ? '-translate-y-full' : 'translate-y-0'} transition-all duration-1000 ease-in-out`}
     >
       <Link href="/">
-        <Image
-          src={'/logo-white.svg'}
-          alt="logo"
-          width={100}
-          height={100}
-        />
+        <Image src={'/logo-white.svg'} alt="logo" width={100} height={100} />
       </Link>
-      <nav className="flex text-xl font-medium">
+      <nav className="text-xl font-medium hidden xs:flex">
         {navbarItems.map((item: NavbarItem) => (
           <Link href={item.href} key={item.name} className="ml-6">
             <p>{item.name}</p>
           </Link>
         ))}
+      </nav>
+      <nav className="block xs:hidden">
+        {isMenuVisible ? (
+          <>
+            <XMarkIcon
+              className="size-6 scale-150 cursor-pointer"
+              onClick={toggleMenu}
+            />
+            <div className="flex flex-col bg-gray-900 absolute right-[4px] top-[70px] rounded-3xl divide-y shadow-md shadow-gray-700">
+              {navbarItems.map((item: NavbarItem) => (
+                <Link href={item.href} key={item.name} className="px-6 py-4 b">
+                  <p>{item.name}</p>
+                </Link>
+              ))}
+            </div>
+          </>
+        ) : (
+          <Bars3Icon
+            className="size-6 scale-150 cursor-pointer"
+            onClick={toggleMenu}
+          />
+        )}
       </nav>
     </header>
   );
