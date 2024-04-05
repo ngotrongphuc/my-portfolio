77f9c177ebffcba7ede64dd29c9d3a9129c63fb7
ngo trong phuc <phucngo4499@gmail.com>
2024-04-05 22:38:04 +0700

    change env url

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -3,7 +3,7 @@ import { Company, ModalIds, NavbarItem, Project, Skill } from './types';
 const BASE_URL =
   process.env.NODE_ENV === 'development'
     ? `http://localhost:${process.env.PORT || 3000}`
-    : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+    : process.env.NEXT_PUBLIC_PROD_URL;
 console.log(BASE_URL);
 
 const navbarItems: NavbarItem[] = [
