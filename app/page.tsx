90e598d23854feda7bdb08e6f32187e25e299e56
ngo trong phuc <phucngo4499@gmail.com>
2024-04-13 21:47:56 +0700

    log ip data

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,16 @@ import Skills from './components/sections/Skills';
 import Work from './components/sections/Work';
 
 export default function Home() {
+  const url = 'http://ip-api.com/json/';
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('IP data:', data);
+    })
+    .catch((error) => {
+      console.error('Error fetching IP data:', error);
+    });
+
   return (
     <div className="min-h-screen min-w-screen">
       <Hero />
